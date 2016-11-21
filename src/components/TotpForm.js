6cba83d0886f7191bf143609af183675f041cef5
Jacob Childress <jacobc@gmail.com>
2016-11-20 19:41:20 -0600

    Add support for setting a TOTP. (Missing file from previous commit.)

---
+++ src/components/TotpForm.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import { Form, Button, Input } from 'semantic-ui-react';
+
+class TotpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: ''
+    };
+    this.setCredentials = this.setCredentials.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  setCredentials(event) {
+    this.props.setTotp(this.state.password);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+        <Form>
+          <Form.Group>
+            <Form.Field>
+              <Input
+                  size="mini"
+                  label="Password"
+                  onChange={this.handlePasswordChange}
+              />
+            </Form.Field>
+            <Button
+                compact primary
+                size="mini"
+                onClick={this.setCredentials}>
+              Set
+            </Button>
+          </Form.Group>
+        </Form>
+    );
+  }
+}
+
+export default TotpForm;
