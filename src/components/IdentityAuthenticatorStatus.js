8136f03dc67ff6a52537ddbfb624d4972d9750d7
Jacob Childress <jacobc@gmail.com>
2016-11-21 18:04:20 -0600

    Don't display a status label if the identity authenticator doesn't have a status value.

--- src/components/IdentityAuthenticatorStatus.js
+++ src/components/IdentityAuthenticatorStatus.js
@@ -3,19 +3,25 @@ import { Label } from 'semantic-ui-react';
 
 class IdentityAuthenticatorStatus extends Component {
   render() {
-    switch(this.props.status) {
-      case 'success':
-        return (
-            <Label color="green" horizontal size="tiny">{this.props.status}</Label>
-        );
-      case 'ready':
-        return (
-            <Label color="orange" horizontal size="tiny">{this.props.status}</Label>
-        );
-      default:
-        return (
-            <Label color="red" horizontal size="tiny">{this.props.status}</Label>
-        );
+    if (this.props.status) {
+      switch(this.props.status) {
+        case 'success':
+          return (
+              <Label color="green" horizontal size="tiny">{this.props.status}</Label>
+          );
+        case 'ready':
+          return (
+              <Label color="orange" horizontal size="tiny">{this.props.status}</Label>
+          );
+        default:
+          return (
+              <Label color="red" horizontal size="tiny">{this.props.status}</Label>
+          );
+      }
+    } else {
+      return (
+          <span/>
+      );
     }
   }
 }
