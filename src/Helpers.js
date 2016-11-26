527ef47ab29c02397c959e085a6c23a69d55ad9d
Jacob Childress <jacobc@gmail.com>
2016-11-25 23:46:22 -0600

    Use const instead of var or let in a couple places.

--- src/Helpers.js
+++ src/Helpers.js
@@ -3,13 +3,13 @@ import URI from 'urijs';
 export function guid() {
   // See: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16|0, v = c === 'x' ? r : ((r&0x3)|0x8);
+    const r = Math.random() * 16|0, v = c === 'x' ? r : ((r&0x3)|0x8);
     return v.toString(16);
   });
 }
 
 export function parseParamsFromUrl(url) {
-  let uri = URI(url);
+  const uri = URI(url);
   let params = {};
   if (uri.query()) {
     params = parseParams(uri.search());
